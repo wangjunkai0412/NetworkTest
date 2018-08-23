f5cc97738495cc1b0aa51f25ddf36312bdb8458f
Wang_junkai <Wang_junkai@aliyun.com>
2018-08-24 00:23:10 +0800

    First commit.

---
+++ app/src/main/java/com/example/networktest/App.java
@@ -0,0 +1,40 @@
+package com.example.networktest;
+
+/**
+ * Created by Xiaomi on 2018-08-24.
+ */
+
+public class App {
+    private String id;
+    private String name;
+    private String version;
+
+    public String getId()
+    {
+        return id;
+    }
+    public void setId()
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName()
+    {
+        this.name = name;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+    public void setVersion()
+    {
+        this.version = version;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.parsers.SAXParserFactory;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView responseText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendRequest = (Button)findViewById(R.id.send_request);
+        responseText = (TextView)findViewById(R.id.response_text);
+
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v.getId()==R.id.send_request)
+        {
+            sendRequestWithOkHttp();
+        }
+    }
+
+    private void sendRequestWithOkHttp()
+    {
+        //开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://www.yishucaiwu.com/get_data.json")
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                //    parseXMLWithPull(responseData);
+                //    parseXMLWithSAX(responseData);
+                //    parseJSONWithJSONObject(responseData);
+                    parseJSONWithGSON(responseData);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void parseJSONWithGSON(String jsonData)
+    {
+        Gson gson = new Gson();
+        List<App> appList = gson.fromJson(jsonData,new TypeToken<List<App>>(){}.getType());
+        for (App app:appList)
+        {
+            Log.d("MainActivity",app.getId());
+            Log.d("MainActivity",app.getName());
+            Log.d("MainActivity",app.getVersion());
+        }
+    }
+
+    private void parseJSONWithJSONObject(String jsonData)
+    {
+        try
+        {
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for (int i=0;i<jsonArray.length();i++)
+            {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String name = jsonObject.getString("name");
+                String version = jsonObject.getString("version");
+                Log.d("MainActivity","id is " + id);
+                Log.d("MainActivity","name is " + name);
+                Log.d("MainActivity","version is " + version);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseXMLWithSAX(String xmlData) {
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
+            ContentHandler handler = new ContentHandler();
+            //将ContentHandler的实例设置到XMLReader中
+            xmlReader.setContentHandler(handler);
+            //开始执行解析
+            xmlReader.parse(new InputSource(new StringReader(xmlData)));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG: {
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:
+                    {
+                        if ("app".equals(nodeName))
+                        {
+                            Log.d("MainActivity","id is " + id);
+                            Log.d("MainActivity","name is " + name);
+                            Log.d("MainActivity","version is " + version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+    private void showResponse(final String response)
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //在这里进行UI操作，将结果显示到界面上
+                responseText.setText(response);
+            }
+        });
+    }
+    */
+}
